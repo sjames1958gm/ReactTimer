a916ce6ea09f341e122d607a30039ca869790d9d
Stephen James <sajames1958@gmail.com>
2016-08-23 01:28:37 +0000

    initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,10 @@
+var React = require("react");
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Countdown</h1>
+    </div>);
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,10 @@
+var React = require("react");
+
+var Timer = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Timer</h1>
+    </div>);
+}
+
+module.exports = Timer;
